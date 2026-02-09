0dd2a2e405efc8dffb87464c8cb2a3e0673068d8
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-02-09 18:18:58 +0530

    Add files via upload

---
+++ # Day 6 - Lists.py
@@ -0,0 +1,11 @@
+# Day 6 - Lists
+fruits = ["apple", "banana", "orange"]
+print("Original list:", fruits)
+
+print("First fruit:", fruits[0])
+print("Last fruit:", fruits[-1])
+fruits.append("mango")
+print("Updated list:", fruits)
+print("Length:", len(fruits))
+
+print("✅ Day 6 Complete!")
