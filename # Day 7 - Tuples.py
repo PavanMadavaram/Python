3e80312cba424cff27728e42b6d6af2b1eb2ed92
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-02-10 15:52:23 +0530

    Add files via upload

---
+++ # Day 7 - Tuples.py
@@ -0,0 +1,8 @@
+# Day 7 - Tuples
+telugu_movies = ("Bahubali", "RRR", "Pushpa", "Ala Vaikuntapuramlo")
+print("Movies:", telugu_movies)
+print("First movie:", telugu_movies[0])
+print("Length:", len(telugu_movies))
+print("Last movie:", telugu_movies[-1])
+
+print("✅ Day 7 Complete!")
