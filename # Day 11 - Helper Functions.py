30b1d139a5bab3b2d05333de394aa3abd3f27cec
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-02-14 20:09:06 +0530

    Add files via upload

---
+++ # Day 11 - Helper Functions.py
@@ -0,0 +1,9 @@
+# Day 11 - Helper Functions
+def add_two_numbers(x, y):
+    return x + y
+
+def multiply_two_numbers(x, y):
+    return x * y
+
+print("10 + 20 =", add_two_numbers(10, 20))
+print("5 × 6 =", multiply_two_numbers(5, 6))
