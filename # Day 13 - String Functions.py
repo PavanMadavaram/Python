a1d1b682c24dddeb3f2e1805c3de2c64ff95bd64
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-02-16 13:10:54 +0530

    Add files via upload

---
+++ # Day 13 - String Functions.py
@@ -0,0 +1,8 @@
+# Day 13 - String Functions
+def reverse_word(word):
+    reversed = word[::-1]
+    print("Original:", word)
+    print("Reversed:", reversed)
+
+reverse_word("python")
+reverse_word("hello")
