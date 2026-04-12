807e0e3bf97149a9d9660244058feb6e1e154242
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-04-12 10:15:17 +0530

    Add files via upload

---
+++ #Day 66 - Random Helper.py
@@ -0,0 +1,10 @@
+#Day 66 - Random Helper
+import random
+
+# Random integers in range
+dice_roll = random.randint(1, 6)
+print("Dice roll:", dice_roll)
+
+# Random float
+random_num = random.uniform(0, 1)
+print("Random float:", random_num)
