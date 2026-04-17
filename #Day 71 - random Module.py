a4f9c7da154ce767009eba01e7d66cdd890d9366
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-04-17 09:18:15 +0530

    Add files via upload

---
+++ #Day 71 - random Module.py
@@ -0,0 +1,10 @@
+#Day 71 - random Module 
+import random
+
+# Random number
+print("Random int:", random.randint(1, 100))
+print("Random float:", random.random())
+
+# Random choice
+colors = ["red", "green", "blue"]
+print("Random color:", random.choice(colors))
