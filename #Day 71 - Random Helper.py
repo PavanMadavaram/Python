bd7682ab9d090fa9f9d94d355d3beb0a67e2f871
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-04-17 09:18:32 +0530

    Add files via upload

---
+++ #Day 71 - Random Helper.py
@@ -0,0 +1,10 @@
+#Day 71 - Random Helper
+import random
+
+# Shuffle list
+items = [1, 2, 3, 4, 5]
+random.shuffle(items)
+print("Shuffled:", items)
+
+# Sample
+print("Sample 2:", random.sample(range(10), 2))
