fe7e5c82f11678b1d19a664d87685a28f751c72c
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-04-28 14:11:13 +0530

    Add files via upload

---
+++ #Day 79 - Itertools Helper.py
@@ -0,0 +1,8 @@
+#Day 79 - Itertools Helper
+import itertools
+
+# Permutations
+print("Permutations:", list(itertools.permutations('AB', 2)))
+
+# Product
+print("Product:", list(itertools.product('AB', repeat=2)))
