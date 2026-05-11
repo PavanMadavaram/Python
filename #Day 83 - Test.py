f617bb9f03f13006334d2c36037329a1d9eb597e
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-05-11 18:36:09 +0530

    Delete #Day 83 - Test.py

--- #Day 83 - Test.py
+++
@@ -1,9 +0,0 @@
-#Day 83 - Test
-import argparse
-import sys
-sys.argv = ['test.py', 'test']
-parser = argparse.ArgumentParser()
-parser.add_argument('test')
-args = parser.parse_args()
-print("Argparse test:", args.test == 'test')
-print("Day 83 test ok")
