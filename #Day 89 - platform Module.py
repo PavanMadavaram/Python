d904959f169093eeb91ae7b39e27b1f12cd9d652
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-05-20 17:40:08 +0530

    Add files via upload

---
+++ #Day 89 - platform Module.py
@@ -0,0 +1,7 @@
+#Day 89 - platform Module 
+import platform
+
+print("System:", platform.system())
+print("Release:", platform.release())
+print("Python:", platform.python_version())
+print("Machine:", platform.machine())
