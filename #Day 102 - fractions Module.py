8072e19df30de59f3b3006f40804c6226809157a
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-06-03 07:46:26 +0530

    Add files via upload

---
+++ #Day 102 - fractions Module.py
@@ -0,0 +1,11 @@
+#Day 102 - fractions Module 
+from fractions import Fraction
+
+a = Fraction(3, 4)
+b = Fraction(5, 6)
+
+print("A:", a)
+print("B:", b)
+print("Add:", a + b)
+print("Multiply:", a * b)
+print("Decimal:", float(a))
