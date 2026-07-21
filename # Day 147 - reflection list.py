2cb22588e2a820bb4e135d1eb46ac86a5b9d9711
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-07-21 17:59:50 +0530

    Add files via upload

---
+++ # Day 147 - reflection list.py
@@ -0,0 +1,10 @@
+# Day 147 - reflection list
+
+reflections = [
+    "What went well today?",
+    "What was challenging?",
+    "What will I do tomorrow?",
+]
+
+for r in reflections:
+    print(r)
