62d27b5634cc2a1baf96b51cd25735db43146d86
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-07-27 10:41:59 +0530

    Add files via upload

---
+++ # Day 153 - bookmark saver.py
@@ -0,0 +1,10 @@
+# Day 153 - bookmark saver
+
+bookmarks = [
+    "https://example.com/python",
+    "https://example.com/docs",
+    "https://example.com/notes",
+]
+
+for link in bookmarks:
+    print(link)
