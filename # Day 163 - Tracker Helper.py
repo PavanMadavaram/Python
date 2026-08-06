3504093586b528faf3ba552dabc166ba40b5b7c3
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-08-06 17:27:55 +0530

    Add files via upload

---
+++ # Day 163 - Tracker Helper.py
@@ -0,0 +1,10 @@
+# Day 163 - Tracker Helper
+
+options = {
+    "A": "Continue",
+    "B": "Pause",
+    "C": "Change direction",
+}
+
+for key, value in options.items():
+    print(f"{key}: {value}")
