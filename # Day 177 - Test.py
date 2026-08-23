9f6605623b438f5c5a9b0accf04b9639543b6cbb
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-08-23 13:53:03 +0530

    Add files via upload

---
+++ # Day 177 - Test.py
@@ -0,0 +1,10 @@
+# Day 177 - Test
+
+def total_focus_time(sessions):
+    return sum(sessions)
+
+
+test_sessions = [20, 30, 10]
+
+print("Day 177 test:", total_focus_time(test_sessions) == 60)
+print("Day 177 test ok")
