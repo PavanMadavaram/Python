7faa01d77d79495e40825ced3255713754403b78
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-08-24 04:32:51 +0530

    Add files via upload

---
+++ # Day 178 - Gratitude Helper.py
@@ -0,0 +1,13 @@
+# Day 178 - Gratitude Helper
+
+def count_entries(journal):
+    return len(journal)
+
+
+journal = [
+    "Finished a tough coding challenge",
+    "Learned a new Python concept",
+    "Stayed consistent with practice",
+]
+
+print("Gratitude entries:", count_entries(journal))
