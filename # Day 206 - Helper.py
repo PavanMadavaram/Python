ee9debdb2567d403de72737387249868859ec142
PAVAN KUMAR MADAVARAM <madavarampavan2002@gmail.com>
2026-09-22 17:55:17 +0530

    Add files via upload

---
+++ # Day 206 - Helper.py
@@ -0,0 +1,21 @@
+# Day 206 - Helper
+
+import heapq
+
+
+def kth_largest(numbers, k):
+    heap = []
+
+    for number in numbers:
+        heapq.heappush(heap, number)
+
+        if len(heap) > k:
+            heapq.heappop(heap)
+
+    return heap[0]
+
+
+values = [7, 10, 4, 3, 20, 15]
+
+print("2nd largest:", kth_largest(values, 2))
+print("4th largest:", kth_largest(values, 4))
